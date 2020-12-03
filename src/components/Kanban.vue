7b6e5749e040327626ffb7abec56c5ca186c74b4
pond004 <anurak.pond12@gmail.com>
2020-12-03 11:02:45 +0700

    edit style

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,5 +1,7 @@
 <template>
-  
+   <div class="kanban">
+       kanban
+   </div>
 </template>
 
 <script>
@@ -9,5 +11,10 @@ export default {
 </script>
 
 <style>
+.kanban{
+    width: 100%;
+    height: 100%;
+    background-color: sandybrown;
+}
 
 </style>
